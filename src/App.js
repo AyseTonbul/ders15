de169e96c64247b2764c3ace68a4a2abfe895adc
Ayse Tonbul <aysetonbul77@hotmail.com>
2022-12-25 19:25:59 +0300

    usecontext fonksiyon gönderme örneği

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
+import React from "react";
+import Puanlar from "./Puanlar";
+import Butonlar from "./Butonlar";
+import { PuanGlobal } from "./Global";
 
 function App() {
+  const [puanlar, puanlariGuncelle] = React.useState({kirmizi:0, yesil:0, turuncu: 0})
+
+
   return (
-   <div>App 1</div>
+    <PuanGlobal.Provider value={{puanlar, puanlariGuncelle}}>
+      <section className="container">
+        <Puanlar />
+        <Butonlar />
+      </section>
+    </PuanGlobal.Provider>
   );
 }
 
