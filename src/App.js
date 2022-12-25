a8fb54c3a2e532d720a773839aea2b3e9662b8bb
Ayse Tonbul <aysetonbul77@hotmail.com>
2022-12-25 21:04:14 +0300

    usereducer örnek tamamlandı

--- src/App.js
+++ src/App.js
@@ -1,7 +1,45 @@
+import React from "react";
+import { varsayilanListe, islemFonksiyonu } from "./reducers/Yapilacaklar";
 
 function App() {
+  const [yeniIs, yeniIsguncelle] = React.useState("")
+  const [yapilacaklarListesi, vekilFonksiyon] = React.useReducer(islemFonksiyonu, varsayilanListe);
+
+  const durumDegistir = (yapilacakIs) => {
+    vekilFonksiyon({ type: "COMPLETE", id: yapilacakIs.id });
+  };
+
+  const isKaydet = () => {
+    vekilFonksiyon({ type: "EKLE", baslik: yeniIs });
+  }
+
+  const isSil = (isId) => {
+    vekilFonksiyon({ type: "SIL", id: isId });
+  }
+
   return (
-   <div>App 1</div>
+    <>
+      <section className="container">
+        <input type="text" value={yeniIs} onChange={(olay) => yeniIsguncelle(olay.target.value)} />
+        <button onClick={isKaydet}>Kaydet</button>
+
+        {yapilacaklarListesi.map((yapilacakIs) => (
+          <div className="d-flex gap-5 justify-content-between" key={yapilacakIs.id}>
+            <label>
+              <input
+                type="checkbox"
+                checked={yapilacakIs.complete}
+                onChange={() => durumDegistir(yapilacakIs)}
+              />
+              {yapilacakIs.title}
+            </label>
+
+            <a onClick={(olay)=>{ olay.preventDefault(); isSil(yapilacakIs.id)  }} href="#">Sil</a>
+          </div>
+        ))}
+
+      </section>
+    </>
   );
 }
 
